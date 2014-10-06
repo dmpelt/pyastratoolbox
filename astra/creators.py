7a3534ed25b94c65c7dfa6e21a4298417ef5a6fb
Daniel M. Pelt <D.M.Pelt@cwi.nl>
2014-10-06 16:58:14 +0200

    Use floats during volume geometry creation

--- astra/creators.py
+++ astra/creators.py
@@ -76,37 +76,37 @@ This method can be called in a number of ways:
     if len(varargin) == 1 and isinstance(varargin[0], int) == 1:
         vol_geom['GridRowCount'] = varargin[0]
         vol_geom['GridColCount'] = varargin[0]
-        vol_geom['option']['WindowMinX'] = -varargin[0] / 2
-        vol_geom['option']['WindowMaxX'] = varargin[0] / 2
-        vol_geom['option']['WindowMinY'] = -varargin[0] / 2
-        vol_geom['option']['WindowMaxY'] = varargin[0] / 2
+        vol_geom['option']['WindowMinX'] = -varargin[0] / 2.
+        vol_geom['option']['WindowMaxX'] = varargin[0] / 2.
+        vol_geom['option']['WindowMinY'] = -varargin[0] / 2.
+        vol_geom['option']['WindowMaxY'] = varargin[0] / 2.
     # astra_create_vol_geom([row_count col_count])
     elif len(varargin) == 1 and len(varargin[0]) == 2:
         vol_geom['GridRowCount'] = varargin[0][0]
         vol_geom['GridColCount'] = varargin[0][1]
-        vol_geom['option']['WindowMinX'] = -varargin[0][1] / 2
-        vol_geom['option']['WindowMaxX'] = varargin[0][1] / 2
-        vol_geom['option']['WindowMinY'] = -varargin[0][0] / 2
-        vol_geom['option']['WindowMaxY'] = varargin[0][0] / 2
+        vol_geom['option']['WindowMinX'] = -varargin[0][1] / 2.
+        vol_geom['option']['WindowMaxX'] = varargin[0][1] / 2.
+        vol_geom['option']['WindowMinY'] = -varargin[0][0] / 2.
+        vol_geom['option']['WindowMaxY'] = varargin[0][0] / 2.
     # astra_create_vol_geom([row_count col_count slice_count])
     elif len(varargin) == 1 and len(varargin[0]) == 3:
         vol_geom['GridRowCount'] = varargin[0][0]
         vol_geom['GridColCount'] = varargin[0][1]
         vol_geom['GridSliceCount'] = varargin[0][2]
-        vol_geom['option']['WindowMinX'] = -varargin[0][1] / 2
-        vol_geom['option']['WindowMaxX'] = varargin[0][1] / 2
-        vol_geom['option']['WindowMinY'] = -varargin[0][0] / 2
-        vol_geom['option']['WindowMaxY'] = varargin[0][0] / 2
-        vol_geom['option']['WindowMinZ'] = -varargin[0][2] / 2
-        vol_geom['option']['WindowMaxZ'] = varargin[0][2] / 2
+        vol_geom['option']['WindowMinX'] = -varargin[0][1] / 2.
+        vol_geom['option']['WindowMaxX'] = varargin[0][1] / 2.
+        vol_geom['option']['WindowMinY'] = -varargin[0][0] / 2.
+        vol_geom['option']['WindowMaxY'] = varargin[0][0] / 2.
+        vol_geom['option']['WindowMinZ'] = -varargin[0][2] / 2.
+        vol_geom['option']['WindowMaxZ'] = varargin[0][2] / 2.
     # astra_create_vol_geom(row_count, col_count)
     elif len(varargin) == 2:
         vol_geom['GridRowCount'] = varargin[0]
         vol_geom['GridColCount'] = varargin[1]
-        vol_geom['option']['WindowMinX'] = -varargin[1] / 2
-        vol_geom['option']['WindowMaxX'] = varargin[1] / 2
-        vol_geom['option']['WindowMinY'] = -varargin[0] / 2
-        vol_geom['option']['WindowMaxY'] = varargin[0] / 2
+        vol_geom['option']['WindowMinX'] = -varargin[1] / 2.
+        vol_geom['option']['WindowMaxX'] = varargin[1] / 2.
+        vol_geom['option']['WindowMinY'] = -varargin[0] / 2.
+        vol_geom['option']['WindowMaxY'] = varargin[0] / 2.
     # astra_create_vol_geom(row_count, col_count, min_x, max_x, min_y, max_y)
     elif len(varargin) == 6:
         vol_geom['GridRowCount'] = varargin[0]
