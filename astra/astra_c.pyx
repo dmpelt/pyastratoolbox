dmpelt/pyastratoolbox

commit bb64d063191aa830028d01b7692fc94d21510439

Added set_gpu_index method from astra-1.4

astra/astra_c.pyx
@@ -33,6 +33,8 @@ cdef extern from "astra/Globals.h" namespace "astra":
     string getVersionString()
     bool cudaEnabled()
 
+cdef extern from "../cuda/2d/darthelper.h" namespace "astraCUDA":
+    bool setGPUIndex(int)
 
 def credits():
     print "All Scale Tomographic Reconstruction Antwerp Toolbox (ASTRA-Toolbox) was developed at the University of Antwerp by"
@@ -59,3 +61,9 @@ def version(printToScreen=False):
         print getVersionString()
     else:
         return getVersion()
+
+def set_gpu_index(idx):
+    if use_cuda()==True:
+        ret = setGPUIndex(idx)
+        if not ret:
+            print "Failed to set GPU " + str(idx)

astra/astra.py
@@ -47,5 +47,12 @@ def version(printToScreen=False):
     :returns: :class:`string` or :class:`int` -- The version string or integer.
     
     """
-    
     return a.version(printToScreen)
+
+def set_gpu_index(idx):
+    """Set default GPU index to use.
+    
+    :param idx: GPU index
+    :type idx: :class:`int`
+    """
+    a.set_gpu_index(idx)

commit f7ee56eb917b0d38de0d71a223bd107407d9570b

Fix compiling for non-CUDA machines caused by bb64d063191aa830028d01b7692fc94d21510439

astra/astra_c.pyx
@@ -36,8 +36,12 @@ cdef extern from "astra/Globals.h" namespace "astra":
     string getVersionString()
     bool cudaEnabled()
 
-cdef extern from "../cuda/2d/darthelper.h" namespace "astraCUDA":
-    bool setGPUIndex(int)
+IF HAVE_CUDA==True:
+  cdef extern from "../cuda/2d/darthelper.h" namespace "astraCUDA":
+      bool setGPUIndex(int)
+ELSE:
+  def setGPUIndex():
+    pass
 
 def credits():
     six.print_("""

builder.py
@@ -40,6 +40,16 @@ try:
 except ImportError:
     use_cython = False
 
+usecuda=False
+try:
+    if os.environ['CPPFLAGS'].find('-DASTRA_CUDA')!=-1:
+        usecuda=True
+except KeyError:
+    pass
+cfg = open('astra/config.pxi','w')
+cfg.write('DEF HAVE_CUDA=' + str(usecuda) + "\n")
+cfg.close()
+
 cmdclass = { }
 ext_modules = [ ]
 
@@ -74,7 +84,7 @@ setup (name = 'PyASTRAToolbox',
        license='GPLv3',
        ext_modules = ext_modules,
        include_dirs=[np.get_include()],
-       cmdclass = cmdclass,       
+       cmdclass = cmdclass,
        #ext_modules = [Extension("astra","astra/astra.pyx")],
        packages=['astra'],
        requires=['numpy'],